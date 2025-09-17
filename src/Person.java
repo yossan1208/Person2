cb887340072becf6efc4e8b717c92916030029ed
yossan1208 <yossan431208@gmail.com>
2025-09-17 22:35:26 +0900

    feat: コンストラクタの演習

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,12 @@
 
 public class Person {
-
+	public String name = null;
+	public int age = 0;
+	
+	public Person() {}
+	
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 }
