bd1ad657882cb72bbffde7daf8c9d69a6fa6eb97
yossan1208 <yossan431208@gmail.com>
2025-09-17 22:44:00 +0900

    feat:　コンストラクタを使ってインスタンス化をする文を追加する。

--- src/Person.java
+++ src/Person.java
@@ -9,4 +9,14 @@ public class Person {
 		this.name = name;
 		this.age = age;
 	}
+	
+	public Person(String name) {
+		this.name = name;
+		this.age = 0;
+	}
+	
+	public Person(int age) {
+		this.name = "名前なし";
+		this.age = age;
+	}
 }
--- src/Test.java
+++ src/Test.java
@@ -11,5 +11,19 @@ public class Test {
 		Person jiro = new Person("jiro", 20);
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
+		
+		System.out.println();
+		
+		Person saburo = new Person("saburo");
+		System.out.println(saburo.name);
+		System.out.println(saburo.age);
+		
+		Person noName = new Person(25);
+		System.out.println(noName.name);
+		System.out.println(noName.age);
+		
+		Person hanako = new Person("hanako", 17);
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
 	}
 }
